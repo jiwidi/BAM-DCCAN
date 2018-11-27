60a4aa7ea8ce0292f0a109d8272efb05b941cb37
Jaime Ferrando Huertas <jaime@Jaimes-MacBook-Pro.local>
2018-11-27 09:45:42 +0100

    added cookiecutter data science project file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A discriminative generative network to generate art images. Trained with the BAM dataset',
+    author='jiwidi',
+    license='MIT',
+)
